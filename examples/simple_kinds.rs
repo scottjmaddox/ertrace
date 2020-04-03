9af6d220a82ba8d9875a4f43035f79055323f67c
Scott J Maddox <scottjmaddox@gmail.com>
2020-04-02 23:34:37 -0500

    add simple_kinds example

---
+++ examples/simple_kinds.rs
@@ -0,0 +1,38 @@
+use ertrace::{ertrace, new_error_type};
+
+fn main() {
+    // On any error in `a`, print the error return trace to stderr,
+    // and then `panic!`.
+    ertrace::try_or_fatal!(a());
+}
+
+fn a() -> Result<(), AError> {
+    // On any error in `b`, return an `AError`, and trace the cause.
+    b().map_err(|e| ertrace!(e => AError))?;
+    Ok(())
+}
+// Define a new traced error type, `AError`.
+new_error_type!(pub struct AError);
+
+fn b() -> Result<(), BError> {
+    // Forward any `BError` errors from `b_inner`.
+    b_inner().map_err(|mut e| ertrace!(e =>))
+}
+
+fn b_inner() -> Result<(), BError> {
+    if true {
+        // Initialize and return a traced error, `BError`,
+        // with error variant `BError1`.
+        Err(ertrace!(BError(BErrorKind::BError1)))
+    } else {
+        // Initialize and return a traced error, `BError`,
+        // with error variant `BError2`.
+        Err(ertrace!(BError(BErrorKind::BError2)))
+    }
+}
+// Define a new traced error type, `BError`, with variant `BErrorKind`.
+new_error_type!(pub struct BError(pub BErrorKind));
+
+// Define the `BError` variants.
+#[derive(Debug)]
+pub enum BErrorKind { BError1, BError2 }
